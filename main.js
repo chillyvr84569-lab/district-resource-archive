81e79a85027e33e981b429ae498bc84626402cca
chillyvr84569-lab <chillyvr84569@gmail.com>
2026-01-29 19:02:35 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,49 @@
-card.onclick = function() {
-            // We are using the URL directly now to avoid 404s
-            const finalUrl = item.url; 
-            
-            if (item.category === "proxy") {
-                window.open(finalUrl, '_blank');
+const container = document.getElementById('game-container');
+const searchBar = document.getElementById('search-bar');
+const panicScreen = document.getElementById('panic-screen');
+let allItems = [];
+
+fetch('games.json')
+    .then(res => res.json())
+    .then(data => {
+        allItems = data;
+        renderCards(allItems);
+    });
+
+function renderCards(data) {
+    container.innerHTML = "";
+    data.forEach(item => {
+        const card = document.createElement('div');
+        card.className = 'game-card';
+        card.innerHTML = `<img src="${item.thumb}"><h3>${item.title}</h3>`;
+        card.onclick = () => {
+            if (item.type === "new") {
+                window.open(item.url, '_blank');
             } else {
-                document.getElementById('game-screen').src = finalUrl;
+                document.getElementById('game-screen').src = item.url;
                 document.getElementById('overlay').classList.remove('hidden');
             }
         };
+        container.appendChild(card);
+    });
+}
+
+// Search Filter
+searchBar.oninput = () => {
+    const query = searchBar.value.toLowerCase();
+    renderCards(allItems.filter(i => i.title.toLowerCase().includes(query)));
+};
+
+// Close Overlay
+document.getElementById('close-btn').onclick = () => {
+    document.getElementById('overlay').classList.add('hidden');
+    document.getElementById('game-screen').src = "";
+};
+
+// PANIC KEY: Press ~ (Tilde) to hide everything
+window.addEventListener('keydown', (e) => {
+    if (e.key === '~') {
+        document.body.innerHTML = `<iframe src="https://docs.google.com/document/d/1_9iE6V0SOnN37Y9Fp_2e_A0l0zF0O0vW/preview" style="width:100%;height:100vh;border:none;"></iframe>`;
+        document.title = "Google Docs";
+    }
+});
